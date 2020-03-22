e81baf1fdea0cf7bbd163e9f4348a686f977d948
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-22 15:52:05 -0400

    added some temp styling, added in models to admin, fixed photo requirement on new tech sign up

--- techs/models.py
+++ techs/models.py
@@ -5,7 +5,7 @@ class Tech(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     position = models.CharField(max_length=75)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
