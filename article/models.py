d15c29a587706d28c0ab361d3813e9b9f741eae1
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-22 12:35:42 -0400

    added in postgres, commented out models, migrated auth and admin models

--- article/models.py
+++ article/models.py
@@ -1,4 +1,4 @@
-# from django.db import models
+from django.db import models
 # from datetime import datetime
 # from envecho.techs.models import Tech
 
--- techs/models.py
+++ techs/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from datetime import datetime
+# from datetime import datetime
 
-class Tech(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    position = models.CharField(max_length=75)
-    description = models.TextField(blank=True)
-    phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=75)
-    hire_date = models.DateTimeField(default=datetime.now, blank=True)
-    def __str__(self):
-        return self.name
+# class Tech(models.Model):
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     username = models.CharField(max_length=200)
+#     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+#     position = models.CharField(max_length=75)
+#     description = models.TextField(blank=True)
+#     phone = models.CharField(max_length=20)
+#     email = models.CharField(max_length=75)
+#     hire_date = models.DateTimeField(default=datetime.now, blank=True)
+#     def __str__(self):
+#         return self.name
