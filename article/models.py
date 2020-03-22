33a8fc719f31eb4079af7c7992e25f8af149f4c6
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-22 13:01:35 -0400

    reviewed models, migrated to DB

--- article/models.py
+++ article/models.py
@@ -1,26 +1,25 @@
 from django.db import models
-# from datetime import datetime
-# from envecho.techs.models import Tech
+from datetime import datetime
+from techs.models import Tech
 
-# class Article(models.Model):
-#     article_tech = models.ForeignKey(Tech, on_delete=models.DO_NOTHING)
-#     article_id = models.AutoField(primary_key=True)
-#     article_date = models.DateTimeField(default=datetime.now, blank=True)
-#     article_name = models.CharField(max_length=250)
-#     article_type = models.CharField(max_length=250)
-#     article_vendor = models.CharField(max_length=250)
-#     article_purchased = models.DateTimeField(default=datetime.now, blank=True)
-#     article_expire = models.DateTimeField(default=datetime.now, blank=True)
-#     article_serial = models.CharField(max_length=250)
-#     article_body = models.TextField(blank=True)
-#     is_published = models.BooleanField(default=True)
-#     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-#     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-#     photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-#     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-#     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-#     def __str__(self):
-#         return self.article_name
+class Article(models.Model):
+    tech = models.ForeignKey(Tech, on_delete=models.DO_NOTHING)
+    create_date = models.DateTimeField(default=datetime.now, blank=True)
+    title = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    vendor = models.CharField(max_length=250)
+    purchase_date = models.DateTimeField(default=datetime.now, blank=True)
+    expire_date = models.DateTimeField(default=datetime.now, blank=True)
+    serial = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    is_published = models.BooleanField(default=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    def __str__(self):
+        return self.title
 
 
     
