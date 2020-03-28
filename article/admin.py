3f0be2cf116b38150b13ff701fd2b6875102e399
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-27 21:13:09 -0400

    customized admin console for techs and articles, added in search and filter fields

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'vendor', 'type', 'expire_date', 'is_published')
+    list_display_links = ('id', 'title')
+    list_filter = ('type', 'vendor')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'vendor', 'type', 'expire_date', 'serial', 'tech')
+    list_per_page = 25
+
+
+
+admin.site.register(Article, ArticleAdmin)
--- techs/admin.py
+++ techs/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Tech
 
-admin.site.register(Tech)
+class TechAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'position', 'email', 'phone')
+    link_display_list = ('first_name', 'last_name')
+    list_filter = ('position',)
+    search_fields = ('first_name', 'last_name', 'username', 'position', 'email', 'phone')
+
+
+admin.site.register(Tech, TechAdmin)
