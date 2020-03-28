081219cf323a83c61767fda2b6b597e1f9601199
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-28 16:33:20 -0400

    added dynamic content to homepage, adjusted navbar to reflect proper navigation

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Article
+
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    articles = Article.objects.order_by('-create_date').filter(is_published=True)[:3]
+
+    context = {
+        'articles': articles
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
