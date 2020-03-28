a994f1c5cc582d23fb2ca0da7564f0f1455e487e
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-28 15:07:01 -0400

    added route to article, added pagination and sort by date, added in is published filtering

--- article/views.py
+++ article/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Article
 
+
 def index(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-create_date').filter(is_published=True)
+
+    paginator = Paginator(articles, 6)
+    page = request.GET.get('page')
+    paged_articles = paginator.get_page(page)
+
     
     context = {
-        'articles': articles
+        'articles': paged_articles
     }
 
     return render(request, 'articles/articles.html', context)
 
-def article(request):
+def article(request, article_id):
     return render(request, 'articles/article.html')
 
 def search(request):
