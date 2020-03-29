da83c74f5fe7ecf7e14d1c7dd150616ba4306f8c
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-29 14:58:06 -0400

    added ins earch queries, added search value persistence in forms

--- article/views.py
+++ article/views.py
@@ -27,4 +27,37 @@ def article(request, article_id):
     return render(request, 'articles/article.html', context)
 
 def search(request):
-    return render(request, 'articles/search.html')
+    queryset_list = Article.objects.order_by('-create_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+    
+    if 'type' in request.GET:
+        type = request.GET['type']
+        if type:
+            queryset_list = queryset_list.filter(type__icontains=type)
+
+    if 'vendor' in request.GET:
+        vendor = request.GET['vendor']
+        if vendor:
+            queryset_list = queryset_list.filter(vendor__icontains=vendor)
+
+    if 'serial' in request.GET:
+        serial = request.GET['serial']
+        if serial:
+            queryset_list = queryset_list.filter(serial__iexact=serial)
+
+    if 'created' in request.GET:
+        created = request.GET['created']
+        if created:
+            queryset_list = queryset_list.filter(create_date__icontains=created)
+
+    context = {
+        'articles': queryset_list,
+        'values': request.GET
+
+    }
+
+    return render(request, 'articles/search.html', context)
